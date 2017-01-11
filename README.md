1f6ccac8c817cc6e7faa83fc9ecffb1c2a3998c2
amar-laksh <zacnethen@gmail.com>
2017-01-12 01:41:25 +0530

    ADDED: BUILD INSTRUCTIONS

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 It's a hobby 32-bit, Monolithic, Multi-boot Kernel which can ofcourse also present an interactive copy of a Resume of its author. :)
 
 
+# BUILD Instructions
+
+Just run the build.sh file and let it do the rest! :)
+(Works on Ubuntu for now)
+
 #Progress
 ![Progress](dekho.gif)
---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo -e "\033c"
+echo "Downloading the i686-elf-4.9.1-Linux-x86_64 cross-compiler..."
+wget http://newos.org/toolchains/i686-elf-4.9.1-Linux-x86_64.tar.xz
+mkdir /root/cross && cd /root/cross/
+tar xvzf i686-elf-4.9.1-Linux-x86_64.tar.xz
+echo "Done. You Now have the cross-compiler installed!"
+echo -e "\033c"
+echo "Downloading the qemu emulator..."
+apt-get install qemu
+echo -e "\033c"
+echo "Done! Please run 'make run' to boot the kernel in qemu! (Look at the Makefile for more help :) )"
