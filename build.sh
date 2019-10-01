970b006dca61a9febf894eec2ce59870dbe57426
amar.lakshya <amar.lakshya@xaviers.edu.in>
2019-10-01 13:34:08 +0100

    major fuck up

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 echo -e "\033c"
 echo "Downloading the i686-elf-4.9.1-Linux-x86_64 cross-compiler..."
+
+mkdir -p ./cross && cd ./cross/
 wget http://newos.org/toolchains/i686-elf-4.9.1-Linux-x86_64.tar.xz
-mkdir /root/cross && cd /root/cross/
 tar xvzf i686-elf-4.9.1-Linux-x86_64.tar.xz
 echo "Done. You Now have the cross-compiler installed!"
 echo -e "\033c"
 echo "Downloading the qemu emulator..."
-apt-get install qemu
+#apt-get install qemu
 echo -e "\033c"
 echo "Done! Please run 'make run' to boot the kernel in qemu!"
